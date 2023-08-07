76f6300ac05be0b20c00180a917c8f133d051294
aminxare <amnxare@gmail.com>
2023-08-08 00:21:25 +0330

    create nextjs action

--- next.config.js
+++ next.config.js
@@ -1,17 +1,35 @@
 const { PHASE_DEVELOPMENT_SERVER } = require("next/constants");
 
 module.exports = (phase) => {
+  const isGithubActions = process.env.GITHUB_ACTIONS || false;
+
+  let assetPrefix = "";
+  let basePath = "/";
+
+  if (isGithubActions) {
+    const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, "");
+
+    assetPrefix = `/${repo}/`;
+    basePath = `/${repo}`;
+  }
+
   if (phase === PHASE_DEVELOPMENT_SERVER)
     return {
       reactStrictMode: true,
-      images:{
-        domains:['images.unsplash.com']
+      images: {
+        domains: ["images.unsplash.com"],
       },
       env: {
         mongodb_url: "mongodb://localhost:27017/nextjs-2-dev",
       },
     };
   return {
+    assetPrefix: assetPrefix,
+    basePath: basePath,
+    images: {
+      loader: 'akamai',
+      path: '',
+    },
     reactStrictMode: true,
     env: {
       mongodb_url: "mongodb://localhost:27017/nextjs-2-production",
